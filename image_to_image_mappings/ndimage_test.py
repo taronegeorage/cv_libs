968440da3bbfacd046e449eaf7567622996bb27b
taronegeorage <1155118584@link.cuhk.edu.hk>
2019-02-18 23:52:46 +0800

    4th chapter

--- image_to_image_mappings/ndimage_test.py
+++ image_to_image_mappings/ndimage_test.py
@@ -16,18 +16,6 @@ def examples():
     imshow(im2)
     show()
 
-def image_in_image(im1,im2,tp):
-    """ Put im1 in im2 with an affine transformation
-    such that corners are as close to tp as possible.
-    tp are homogeneous and counter-clockwise from top left. """
-    
-    # points to warp from
-    m,n = im1.shape[:2]
-    fp = np.array([[0,m,m,0],[0,0,n,n],[1,1,1,1]])
-    # compute affine transform and apply
-    H = homography.Haffine_from_points(tp,fp)
-    im1_t = ndimage.affine_transform(im1,H[:2,:2],(H[0,2],H[1,2]),im2.shape[:2])
-    alpha = (im1_t > 0)
-    return (1-alpha)*im2 + alpha*im1_t
+
 
 
